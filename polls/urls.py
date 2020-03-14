8e696d8c8489cc3b11922b37e46ddb8203e590fa
Christoph Brosch <me@pop-os.localdomain>
2020-03-14 22:43:27 +0100

    finished chapter 7

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,32 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name="index"),
+    path(
+        '', 
+        # views.index,
+        views.IndexView.as_view(), 
+        name="index"
+        ),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    # The URL is not hardcoded into the template, the template references the URL with the name attribute
+    path(
+        # This URL can be changed to anything e.g "specific/<int:pk> no problemo"
+        "<int:pk>/",
+        # views.detail,
+        views.DetailView.as_view(), 
+        name="detail"
+        ),
+
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.result, name="results"),
+    path(
+        "<int:pk>/results/", 
+        # views.result, 
+        views.ResultsView.as_view(),
+        name="results"
+        ),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
